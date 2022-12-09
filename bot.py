675ed16a69d5d82376fd43b509f6fa24a07f8b6f
Максим Калинин <maksim.kalinin1@gmail.com>
2022-12-09 14:53:21 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,52 @@
+import requests
+import os
+import telegram
+import time
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    dvmn_token = os.getenv("DVMN_TOKEN")
+    tg_token = os.getenv("TG_BOT_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    bot = telegram.Bot(token=tg_token)
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {
+        "Authorization": f"Token {dvmn_token}",
+    }
+    payload = {}
+    while True:
+        try:
+            response = requests.get(
+                url,
+                headers=headers,
+                params=payload,
+                timeout=5,
+             )
+            response.raise_for_status()
+            if response.json()['status'] == 'found':
+                lesson_title = response.json()['new_attempts'][0]['lesson_title']
+                bot.send_message(
+                    chat_id=chat_id,
+                    text=f"Преподаватель проверил работу '{lesson_title}'!",
+                 )
+                if response.json()['new_attempts'][0]['is_negative']:
+                    lesson_url = response.json()['new_attempts'][0]['lesson_url']
+                    bot.send_message(
+                        chat_id=chat_id,
+                        text=f"К сожалению в работе'{lesson_url}' нашлись ошибки!",
+                     )
+                else:
+                    bot.send_message(
+                        chat_id=chat_id,
+                        text="Преподавателю все понравилось!",
+                     )
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            print('Произошел разрыв сетевого соединения. Ожидаем 10 секунд.')
+            time.sleep(10)
+            continue
+        timestamp = response.json()['last_attempt_timestamp']
+        payload = {"timestamp": timestamp}
