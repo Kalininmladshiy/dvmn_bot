42980824783f6834e73ae931507f27d42d2103da
Максим Калинин <maksim.kalinin1@gmail.com>
2022-12-16 17:04:40 +0300

    send logs to Telegram to admin user

--- bot.py
+++ bot.py
@@ -2,15 +2,42 @@ import requests
 import os
 import telegram
 import time
+import logging
 from dotenv import load_dotenv
 
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_logger_token, tg_chat_id):
+        super().__init__()
+        self.tg_chat_id = tg_chat_id
+        self.tg_logger_token = tg_logger_token
+        self.bot_logger = telegram.Bot(token=self.tg_logger_token)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot_logger.send_message(chat_id=self.tg_chat_id, text=log_entry)
 
 if __name__ == '__main__':
     load_dotenv()
     dvmn_token = os.getenv("DVMN_TOKEN")
     tg_token = os.getenv("TG_BOT_TOKEN")
+    tg_logger_token = os.getenv("TG_BOT_LOGGER_TOKEN")
     tg_chat_id = os.getenv("TG_CHAT_ID")
+
     bot = telegram.Bot(token=tg_token)
+    
+    logger = logging.getLogger('dvmn_bot')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(process)d %(levelname)s %(filename)s %(lineno)d %(asctime)s %(message)s"
+     )
+    tg_handler = TelegramLogsHandler(tg_logger_token, tg_chat_id)
+    tg_handler.setLevel(logging.DEBUG)
+    tg_handler.setFormatter(formatter)
+    logger.addHandler(tg_handler)
+    logger.debug('Бот запущен')
+
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         "Authorization": f"Token {dvmn_token}",
@@ -29,6 +56,7 @@ if __name__ == '__main__':
             if task['status'] == 'found':
                 timestamp = task['last_attempt_timestamp']
                 lesson_title = task['new_attempts'][0]['lesson_title']
+                logger.info('Работа проверена')
                 bot.send_message(
                     chat_id=tg_chat_id,
                     text=f"Преподаватель проверил работу '{lesson_title}'!",
@@ -48,8 +76,8 @@ if __name__ == '__main__':
                 timestamp = task['timestamp_to_request']
         except requests.exceptions.ReadTimeout:
             continue
-        except requests.exceptions.ConnectionError:
-            print('Произошел разрыв сетевого соединения. Ожидаем 10 секунд.')
+        except requests.exceptions.ConnectionError as err:
+            logging.error(err, exc_info=True)
             time.sleep(10)
             continue
         payload = {"timestamp": timestamp}
